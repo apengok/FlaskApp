59964858a00676eca9ddf8d98b5d95318347d9aa
root <root@scadatest-centos6.3-32>
2017-06-06 16:40:42 +0800

    static and template

--- __init__.py
+++ __init__.py
@@ -1,4 +1,7 @@
 from flask import Flask,render_template
+from content_management import Content
+
+TOPIC_DICT = Content()
 
 app = Flask(__name__)
 
@@ -6,6 +9,14 @@ app = Flask(__name__)
 def homepage():
     return render_template("main.html")
 
+@app.route('/dashboard/')
+def dashboard():
+    return render_template("dashboard.html",topics=TOPIC_DICT)
+
+    
+@app.errorhandler(500)
+def page_not_found(e):
+    return "Oops,looks like we're knocked out",500
 
 if __name__ == "__main__":
     app.run()
